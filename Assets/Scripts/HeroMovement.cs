30f5aed1375586da9c52b8d7147ca34aa25d9c4c
Rafael Nicholas Po <163509148+rafpoo@users.noreply.github.com>
2025-10-31 15:37:10 +0700

    feat: lab - tutorial done

--- Assets/Scripts/HeroMovement.cs
+++ Assets/Scripts/HeroMovement.cs
@@ -4,53 +4,40 @@ using UnityEngine;
 
 public class Hero : MonoBehaviour
 {
-    Vector3 maju = Vector3.zero;
-    float kecepatan = 3f;
-    // Start is called before the first frame update
+    private Animator anim;
+
+    // membuat IK foot
+    [Range(0, 1f)] public float distanceToGround;
+    public LayerMask layerMask;
     void Start()
     {
-
+        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        float h = Input.GetAxis("Horizontal");
-        float v = Input.GetAxis("Vertical");
+        if (Input.GetKeyDown(KeyCode.Space))
+            anim.SetBool("StatJalan", !anim.GetBool("StatJalan"));
+    }
 
-        if (h != 0 || v != 0)
+    private void OnAnimatorIK(int layerIndex)
+    {
+        // left foot
+        anim.SetIKPositionWeight(AvatarIKGoal.LeftFoot, 1);
+        anim.SetIKRotationWeight(AvatarIKGoal.LeftFoot, 1);
+
+        RaycastHit hit;
+        Ray ray = new Ray(anim.GetIKPosition(AvatarIKGoal.LeftFoot) + Vector3.up, Vector3.down);
+        if (Physics.Raycast(ray, out hit, distanceToGround + 1f, layerMask))
         {
-            Vector3 targetDirection = new Vector3(h, 0f, v);
-            targetDirection = Camera.main.transform.TransformDirection(targetDirection);
-            targetDirection.y = 0.0f;
-
-            Quaternion targetRotation = Quaternion.LookRotation(targetDirection, Vector3.up);
-            transform.rotation = targetRotation;
-
-            Vector3 moveForward = Vector3.ProjectOnPlane(Camera.main.transform.forward, Vector3.up);
-            moveForward.y = 0;
-            moveForward.Normalize();
-
-            Vector3 moveRight = Vector3.ProjectOnPlane(Camera.main.transform.right, Vector3.up);
-            moveRight.y = 0;
-            moveRight.Normalize();
-
-            transform.position += (moveForward * v + moveRight * h) * Time.deltaTime * kecepatan;
-
-            GetComponent<Animator>().SetBool("StatJalan", true);
-        }
-        else
-        {
-            GetComponent<Animator>().SetBool("StatJalan", false);
-        }
-
-        if (Input.GetMouseButtonDown(1))
-        {
-            GetComponent<Animator>().SetBool("StatAim", true);
-        }
-        if (Input.GetMouseButtonUp(1))
-        {
-            GetComponent<Animator>().SetBool("StatAim", false);
+            if (hit.transform.tag == "walkable")
+            {
+                Vector3 footPosition = hit.point;
+                footPosition.y += distanceToGround;
+                anim.SetIKPosition(AvatarIKGoal.LeftFoot, footPosition);
+                // anim.SetIKRotation(AvatarIKGoal.LeftFoot, Quaternion.LookRotation(transform.forward, hit.normal));
+            }
         }
     }
 }
