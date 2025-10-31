beac2dc041ac66150c9114980882033c5bf9fec9
Rafael Nicholas Po <163509148+rafpoo@users.noreply.github.com>
2025-10-31 13:57:48 +0700

    feat: lec - challenge 2 - done

--- Assets/Scripts/LaserScript.cs
+++ Assets/Scripts/LaserScript.cs
@@ -5,16 +5,23 @@ using UnityEngine;
 public class LaserScript : MonoBehaviour
 {
     private LineRenderer lr;
+    private Animator heroAnimator;
 
     // Start is called before the first frame update
     void Start()
     {
+        heroAnimator = GameObject.Find("hero").GetComponent<Animator>();
         lr = GetComponent<LineRenderer>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        bool aiming = heroAnimator.GetBool("StatAim");
+        lr.enabled = aiming;
+
+        if (!aiming) return;
+
         lr.SetPosition(0, transform.position);
 
         RaycastHit hit;
